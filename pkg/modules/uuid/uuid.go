90f30e2bf10d06d08c0a68412282abc7e77e8fe5
Antoine GIRARD <sapk@users.noreply.github.com>
2017-05-17 18:00:51 +0200

    Update uuid.go

--- pkg/modules/uuid/uuid.go
+++ pkg/modules/uuid/uuid.go
@@ -5,10 +5,12 @@ import (
 
 	uuid "github.com/nu7hatch/gouuid"
 	"github.com/sapk/sca/pkg/model"
+	"github.com/spf13/pflag"
 	log "github.com/sirupsen/logrus"
 )
 
 const ModuleID = "UUID"
+string argUUID
 
 //Module retrieve information form executing sca
 type Module struct {
@@ -18,27 +20,37 @@ type Module struct {
 //Response describe collector informations
 type Response string
 
+//Flags set for Module
+func Flags() *pflag.FlagSet {
+	fSet := pflag.NewFlagSet(ModuleID, pflag.ExitOnError)
+	fSet.StringVar(&argUUID, "uuid", "", "uuid to use by this collector")
+	return fSet
+}
+
 //New constructor for Module
 func New(options map[string]string) model.Module {
 	log.WithFields(log.Fields{
 		"id":      ModuleID,
 		"options": options,
 	}).Debug("Creating new Module")
-	hostname, err := os.Hostname()
-	if err != nil {
-		log.WithFields(log.Fields{
-			"hostname": hostname,
-			"err":      err,
-		}).Warn("Failed to retrieve hostname")
+	if argUUID == "" {
+		hostname, err := os.Hostname()
+		if err != nil {
+			log.WithFields(log.Fields{
+				"hostname": hostname,
+				"err":      err,
+			}).Warn("Failed to retrieve hostname")
+		}
+		u5, err := uuid.NewV5(uuid.NamespaceURL, []byte(hostname)) //TODO better discriminate maybe add time and save it in /etc/sca/uuid ?
+		if err != nil {
+			log.WithFields(log.Fields{
+				"uuid": u5,
+				"err":  err,
+			}).Fatal("Failed to generate uuid")
+		}
+		argUUID = u5.String()
 	}
-	u5, err := uuid.NewV5(uuid.NamespaceURL, []byte(hostname)) //TODO better discriminate maybe add time and save it in /etc/sca/uuid ?
-	if err != nil {
-		log.WithFields(log.Fields{
-			"uuid": u5,
-			"err":  err,
-		}).Fatal("Failed to generate uuid")
-	}
-	return &Module{UUID: u5.String()} //TODO use option to get a user or config (/etc/sca/uuid or via cmd ?) defined uuid
+	return &Module{UUID: argUUID}
 }
 //ID //TODO
 func (m *Module) ID() string {
