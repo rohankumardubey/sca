e5eb0c450c01e5cac76804e97142b39be5d463da
Antoine GIRARD <sapk@users.noreply.github.com>
2017-05-19 16:46:57 +0200

    Update module.go

--- pkg/model/module.go
+++ pkg/model/module.go
@@ -1,8 +1,12 @@
 package model
 
+import "github.com/spf13/pflag"
+
 //Module represente a module collecting data //TODO add a Close method tha close chan
 type Module interface {
 	ID() string
+	New() Module
+	Flags() *pflag.FlagSet
 	Event() <-chan string
 	GetData() interface{}
 }
